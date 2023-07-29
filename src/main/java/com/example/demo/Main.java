f09a80a0e38edd1c10d89ba2f95d1fbd90138fe9
QuizMJD <darkscopio2@mail.com>
2023-07-29 14:21:54 +0700

    l2

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -4,35 +4,78 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void searchBrandByName(Connection conn, String searchKeyword) throws SQLException {
+    // Method to create the connection and set up database resources
+    private static Connection createConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/java30", "root", "123456");
+    }
+
+    // Method to close the connection and other resources
+    private static void closeResources(Connection conn, Statement stm, ResultSet rs) {
+        try {
+            if (rs != null) rs.close();
+            if (stm != null) stm.close();
+            if (conn != null) conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to insert a new brand into the database
+    private static int insertBrand(Connection conn, String name) throws SQLException {
+        String insertQuery = "INSERT INTO brands (name) VALUES (?)";
+        PreparedStatement insertStatement = conn.prepareStatement(insertQuery);
+        insertStatement.setString(1, name);
+        return insertStatement.executeUpdate();
+    }
+
+    // Method to update the name of a brand based on its ID
+    private static int updateBrandName(Connection conn, int id, String newName) throws SQLException {
+        String updateQuery = "UPDATE brands SET name = ? WHERE id = ?";
+        PreparedStatement updateStatement = conn.prepareStatement(updateQuery);
+        updateStatement.setString(1, newName);
+        updateStatement.setInt(2, id);
+        return updateStatement.executeUpdate();
+    }
+
+    // Method to delete a brand based on its ID
+    private static int deleteBrandById(Connection conn, int id) throws SQLException {
+        String deleteQuery = "DELETE FROM brands WHERE id = ?";
+        PreparedStatement deleteStatement = conn.prepareStatement(deleteQuery);
+        deleteStatement.setInt(1, id);
+        return deleteStatement.executeUpdate();
+    }
+
+    // Method to search for a brand by its name
+    private static void searchBrandByName(Connection conn, String searchKeyword) throws SQLException {
         String searchQuery = "SELECT * FROM brands WHERE name LIKE ?";
         PreparedStatement searchStatement = conn.prepareStatement(searchQuery);
         searchStatement.setString(1, "%" + searchKeyword + "%");
         ResultSet rs = searchStatement.executeQuery();
 
-        // Hiển thị kết quả tìm kiếm
+        // Display search results
         while (rs.next()) {
             int id = rs.getInt("id");
             String name = rs.getString("name");
-            System.out.println("ID: " + id + ", Tên: " + name);
+            System.out.println("ID: " + id + ", Name: " + name);
         }
     }
-    public static void main(String[] args){
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("driver loading");
-            Connection conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/java30", "root", "123456");
-            System.out.println("connet db");
-            Statement stm = conn.createStatement();
-//            stm.execute("create table if not exists brands(id int primary key auto_increment,name varchar(50))");
-            int affectRow= stm.executeUpdate("insert into brands(name) values('nike')");
-            System.out.println(affectRow);
-            stm.executeUpdate("insert into brands(name) values('puma')");
-            stm.executeUpdate("insert into brands(name) values('çonver')");
 
-            // Truy vấn để lấy dữ liệu từ bảng "brands"
+    public static void main(String[] args) {
+        try (Connection conn = createConnection();
+             Statement stm = conn.createStatement()) {
+
+            System.out.println("Connected to the database");
+
+            // Insert new brands
+            int rowsInserted = insertBrand(conn, "nike");
+            System.out.println("Rows inserted: " + rowsInserted);
+
+            insertBrand(conn, "puma");
+            insertBrand(conn, "conver");
+
+            // Retrieve all brands
             ResultSet rs = stm.executeQuery("SELECT * FROM brands");
-            // Hiển thị dữ liệu lên console
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
@@ -40,35 +83,22 @@ public class Main {
             }
 
             // Assume we want to update the name of the brand with ID 1
-            String updateQuery = "UPDATE brands SET name = ? WHERE id = ?";
-            PreparedStatement updateStatement = conn.prepareStatement(updateQuery);
-            updateStatement.setString(1, "new_brand_name");
-            updateStatement.setInt(2, 1);
-            int rowsUpdated = updateStatement.executeUpdate();
+            int rowsUpdated = updateBrandName(conn, 1, "new_brand_name");
             System.out.println("Rows updated: " + rowsUpdated);
+
             // Assume we want to delete the brand with ID 3
-            String deleteQuery = "DELETE FROM brands WHERE id = ?";
-            PreparedStatement deleteStatement = conn.prepareStatement(deleteQuery);
-            deleteStatement.setInt(1, 3);
-            int rowsDeleted = deleteStatement.executeUpdate();
+            int rowsDeleted = deleteBrandById(conn, 3);
             System.out.println("Rows deleted: " + rowsDeleted);
-            // Mã đã có sẵn cho việc kết nối cơ sở dữ liệu và thêm dữ liệu ...
 
-            // Tạo đối tượng Scanner để đọc dữ liệu nhập từ bàn phím
+            // Create a Scanner object to read user input
             Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Nhập tên thương hiệu cần tìm kiếm:");
+            System.out.println("Enter the brand name to search:");
             String searchKeyword = scanner.nextLine();
 
-            // Thực hiện tìm kiếm dựa trên dữ liệu nhập từ người dùng
+            // Perform the search based on user input
             searchBrandByName(conn, searchKeyword);
 
-            // Đóng đối tượng Scanner và kết nối...
-            scanner.close();
-            conn.close();
-
-
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
